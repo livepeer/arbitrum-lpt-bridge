f45e494467ffdf98396251079bf5a8fb3fb84169
kautuk kundan <kautukkundan@gmail.com>
2021-11-17 21:30:24 +0530

    added contract methods for outbound transfer added tests

--- contracts/L2/gateway/IL2LPTGateway.sol
+++ contracts/L2/gateway/IL2LPTGateway.sol
@@ -9,6 +9,22 @@ interface IL2LPTGateway {
         uint256 _amount
     );
 
+    event WithdrawalInitiated(
+        address _l1Token,
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _l2ToL1Id,
+        uint256 _exitNum,
+        uint256 _amount
+    );
+
+    function outboundTransfer(
+        address _token,
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) external returns (bytes memory);
+
     function finalizeInboundTransfer(
         address _token,
         address _from,
@@ -16,4 +32,13 @@ interface IL2LPTGateway {
         uint256 _amount,
         bytes calldata _data
     ) external;
+
+    // if token is not supported this should return 0x0 address
+    function calculateL2TokenAddress(address _l1Token)
+        external
+        view
+        returns (address);
+
+    // used by router
+    function counterpartGateway() external view returns (address);
 }
