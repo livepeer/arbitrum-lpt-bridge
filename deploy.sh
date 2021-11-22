73470ed0c583f7590560688b124ec88fcace375b
kautuk kundan <kautukkundan@gmail.com>
2021-11-23 01:17:41 +0530

    initial deployment on testnet

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-echo -e "Deploying L1 Token and Bridge"
+echo -e "Deploying L1 Token and Escrow"
+npx hardhat deploy --tags L1_ESCROW --network rinkeby
+
+echo -e "Deploying L2 Token"
+npx hardhat deploy --tags L2_TOKEN --network arbitrumRinkeby
+
+echo -e "\n Deploying L1 Bridge"
 npx hardhat deploy --tags L1_GATEWAY --network rinkeby
 
-echo -e "\n Deploying L2 Token and Bridge"
+echo -e "\n Deploying L2 Bridge"
 npx hardhat deploy --tags L2_GATEWAY --network arbitrumRinkeby
 
 echo -e "\n Initialize L1 Bridge"
