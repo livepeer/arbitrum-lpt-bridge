fa83d30a087c6f2a0a3b0d54715dce4da709428c
Yondon Fu <yondon.fu@gmail.com>
2022-02-13 20:36:42 -0500

    tasks: Add estimatel2 flag for initial migration tasks

--- tasks/cache-total-supply.ts
+++ tasks/cache-total-supply.ts
@@ -9,10 +9,18 @@ import {
 import {getArbitrumContracts} from '../deploy/helpers';
 import {EthersProviderWrapper} from '../deploy/ethers-provider-wrapper';
 
-task('cache-total-supply', 'Cache total supply on L2').setAction(
-    async (_, hre) => {
+task('cache-total-supply', 'Cache total supply on L2')
+    .addOptionalParam('refund', '(Optional) Refund address on L2')
+    .addOptionalParam(
+        'estimatel2',
+        '(Optional) Set to true to only estimate L2 gas params',
+    )
+    .setAction(async (taskArgs, hre) => {
       const {getNamedAccounts, deployments, ethers} = hre;
-      const {deployer} = await getNamedAccounts();
+      let {deployer} = await getNamedAccounts();
+      if (taskArgs.refund) {
+        deployer = taskArgs.refund;
+      }
 
       const l2Provider = new EthersProviderWrapper(
           hre.companionNetworks['l2'].provider,
@@ -45,6 +53,14 @@ task('cache-total-supply', 'Cache total supply on L2').setAction(
       );
       const ethValue = maxSubmissionCost.add(gasPriceBid.mul(maxGas));
 
+      if (taskArgs.estimatel2) {
+        console.log(`maxGas: ${maxGas.toString()}`);
+        console.log(`gasPriceBid: ${gasPriceBid.toString()}`);
+        console.log(`maxSubmissionCost: ${maxSubmissionCost.toString()}`);
+        console.log(`value: ${ethValue.toString()}`);
+        return;
+      }
+
       await waitToRelayTxsToL2(
           waitForTx(
               cache.cacheTotalSupply(maxGas, gasPriceBid, maxSubmissionCost, {
@@ -55,5 +71,4 @@ task('cache-total-supply', 'Cache total supply on L2').setAction(
           ethers.provider,
           l2Provider,
       );
-    },
-);
+    });
