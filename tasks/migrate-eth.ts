c62d90ac1d42a86bfad877a1121174ca3967c7e8
Yondon Fu <yondon.fu@gmail.com>
2022-02-13 20:36:42 -0500

    tasks: Add estimatel2 flag for initial migration tasks

--- tasks/migrate-eth.ts
+++ tasks/migrate-eth.ts
@@ -9,45 +9,58 @@ import {
 import {getArbitrumContracts} from '../deploy/helpers';
 import {EthersProviderWrapper} from '../deploy/ethers-provider-wrapper';
 
-task('migrate-eth', 'Migrate ETH to L2').setAction(async (_, hre) => {
-  const {deployments, ethers} = hre;
+task('migrate-eth', 'Migrate ETH to L2')
+    .addOptionalParam('estimatel2', 'Set to true to only estimate L2 gas params')
+    .setAction(async (taskArgs, hre) => {
+      const {deployments, ethers} = hre;
 
-  const l2Provider = new EthersProviderWrapper(
-      hre.companionNetworks['l2'].provider,
-  );
+      const l2Provider = new EthersProviderWrapper(
+          hre.companionNetworks['l2'].provider,
+      );
 
-  const l1MigratorDeployment = await deployments.get('L1Migrator');
-  const l2MigratorDeployment = await hre.companionNetworks[
-      'l2'
-  ].deployments.get('L2MigratorProxy');
+      const l1MigratorDeployment = await deployments.get('L1Migrator');
+      const l2MigratorDeployment = await hre.companionNetworks[
+          'l2'
+      ].deployments.get('L2MigratorProxy');
 
-  const migrator = await ethers.getContractAt(
-      'L1Migrator',
-      l1MigratorDeployment.address,
-  );
+      const migrator = await ethers.getContractAt(
+          'L1Migrator',
+          l1MigratorDeployment.address,
+      );
 
-  const l2Calldata = '0x';
-  const gasPriceBid = await getGasPriceBid(l2Provider);
-  const maxSubmissionCost = await getMaxSubmissionPrice(l2Provider, l2Calldata);
-  const maxGas = await getMaxGas(
-      l2Provider,
-      migrator.address,
-      l2MigratorDeployment.address,
-      migrator.address,
-      maxSubmissionCost,
-      gasPriceBid,
-      l2Calldata,
-  );
-  const ethValue = maxSubmissionCost.add(gasPriceBid.mul(maxGas));
+      const l2Calldata = '0x';
+      const gasPriceBid = await getGasPriceBid(l2Provider);
+      const maxSubmissionCost = await getMaxSubmissionPrice(
+          l2Provider,
+          l2Calldata,
+      );
+      const maxGas = await getMaxGas(
+          l2Provider,
+          migrator.address,
+          l2MigratorDeployment.address,
+          migrator.address,
+          maxSubmissionCost,
+          gasPriceBid,
+          l2Calldata,
+      );
+      const ethValue = maxSubmissionCost.add(gasPriceBid.mul(maxGas));
 
-  await waitToRelayTxsToL2(
-      waitForTx(
-          migrator.migrateETH(maxGas, gasPriceBid, maxSubmissionCost, {
-            value: ethValue,
-          }),
-      ),
-      getArbitrumContracts(hre.network.name).inbox,
-      ethers.provider,
-      l2Provider,
-  );
-});
+      if (taskArgs.estimatel2) {
+        console.log(`maxGas: ${maxGas.toString()}`);
+        console.log(`gasPriceBid: ${gasPriceBid.toString()}`);
+        console.log(`maxSubmissionCost: ${maxSubmissionCost.toString()}`);
+        console.log(`value: ${ethValue.toString()}`);
+        return;
+      }
+
+      await waitToRelayTxsToL2(
+          waitForTx(
+              migrator.migrateETH(maxGas, gasPriceBid, maxSubmissionCost, {
+                value: ethValue,
+              }),
+          ),
+          getArbitrumContracts(hre.network.name).inbox,
+          ethers.provider,
+          l2Provider,
+      );
+    });
--- tasks/migrate-lpt.ts
+++ tasks/migrate-lpt.ts
@@ -9,71 +9,84 @@ import {
 import {getArbitrumContracts} from '../deploy/helpers';
 import {EthersProviderWrapper} from '../deploy/ethers-provider-wrapper';
 
-task('migrate-lpt', 'Migrate LPT to L2').setAction(async (taskArgs, hre) => {
-  const {deployments, ethers} = hre;
+task('migrate-lpt', 'Migrate LPT to L2')
+    .addOptionalParam('estimatel2', 'Set to true to only estimate L2 gas params')
+    .setAction(async (taskArgs, hre) => {
+      const {deployments, ethers} = hre;
 
-  const l2Provider = new EthersProviderWrapper(
-      hre.companionNetworks['l2'].provider,
-  );
+      const l2Provider = new EthersProviderWrapper(
+          hre.companionNetworks['l2'].provider,
+      );
 
-  const l1MigratorDeployment = await deployments.get('L1Migrator');
-  const l2MigratorDeployment = await hre.companionNetworks[
-      'l2'
-  ].deployments.get('L2MigratorProxy');
+      const l1MigratorDeployment = await deployments.get('L1Migrator');
+      const l2MigratorDeployment = await hre.companionNetworks[
+          'l2'
+      ].deployments.get('L2MigratorProxy');
 
-  const l1GatewayDeployment = await deployments.get('L1LPTGateway');
-  const l2GatewayDeployment = await hre.companionNetworks['l2'].deployments.get(
-      'L2LPTGateway',
-  );
+      const l1GatewayDeployment = await deployments.get('L1LPTGateway');
+      const l2GatewayDeployment = await hre.companionNetworks[
+          'l2'
+      ].deployments.get('L2LPTGateway');
 
-  const migrator = await ethers.getContractAt(
-      'L1Migrator',
-      l1MigratorDeployment.address,
-  );
+      const migrator = await ethers.getContractAt(
+          'L1Migrator',
+          l1MigratorDeployment.address,
+      );
 
-  const gateway = await ethers.getContractAt(
-      'L1LPTGateway',
-      l1GatewayDeployment.address,
-  );
+      const gateway = await ethers.getContractAt(
+          'L1LPTGateway',
+          l1GatewayDeployment.address,
+      );
 
-  const minterAddr = await migrator.bridgeMinterAddr();
-  const tokenAddr = await migrator.tokenAddr();
+      const minterAddr = await migrator.bridgeMinterAddr();
+      const tokenAddr = await migrator.tokenAddr();
 
-  const tokenABI = [
-    'function balanceOf(address) public view returns (uint256)',
-  ];
-  const token = await ethers.getContractAt(tokenABI, tokenAddr);
+      const tokenABI = [
+        'function balanceOf(address) public view returns (uint256)',
+      ];
+      const token = await ethers.getContractAt(tokenABI, tokenAddr);
 
-  const amount = await token.balanceOf(minterAddr);
+      const amount = await token.balanceOf(minterAddr);
 
-  const l2Calldata = await gateway.getOutboundCalldata(
-      token.address,
-      migrator.address,
-      l2MigratorDeployment.address,
-      amount,
-      '0x',
-  );
-  const gasPriceBid = await getGasPriceBid(l2Provider);
-  const maxSubmissionCost = await getMaxSubmissionPrice(l2Provider, l2Calldata);
-  const maxGas = await getMaxGas(
-      l2Provider,
-      gateway.address,
-      l2GatewayDeployment.address,
-      migrator.address,
-      maxSubmissionCost,
-      gasPriceBid,
-      l2Calldata,
-  );
-  const ethValue = maxSubmissionCost.add(gasPriceBid.mul(maxGas));
+      const l2Calldata = await gateway.getOutboundCalldata(
+          token.address,
+          migrator.address,
+          l2MigratorDeployment.address,
+          amount,
+          '0x',
+      );
+      const gasPriceBid = await getGasPriceBid(l2Provider);
+      const maxSubmissionCost = await getMaxSubmissionPrice(
+          l2Provider,
+          l2Calldata,
+      );
+      const maxGas = await getMaxGas(
+          l2Provider,
+          gateway.address,
+          l2GatewayDeployment.address,
+          migrator.address,
+          maxSubmissionCost,
+          gasPriceBid,
+          l2Calldata,
+      );
+      const ethValue = maxSubmissionCost.add(gasPriceBid.mul(maxGas));
 
-  await waitToRelayTxsToL2(
-      waitForTx(
-          migrator.migrateLPT(maxGas, gasPriceBid, maxSubmissionCost, {
-            value: ethValue,
-          }),
-      ),
-      getArbitrumContracts(hre.network.name).inbox,
-      ethers.provider,
-      l2Provider,
-  );
-});
+      if (taskArgs.estimatel2) {
+        console.log(`maxGas: ${maxGas.toString()}`);
+        console.log(`gasPriceBid: ${gasPriceBid.toString()}`);
+        console.log(`maxSubmissionCost: ${maxSubmissionCost.toString()}`);
+        console.log(`value: ${ethValue.toString()}`);
+        return;
+      }
+
+      await waitToRelayTxsToL2(
+          waitForTx(
+              migrator.migrateLPT(maxGas, gasPriceBid, maxSubmissionCost, {
+                value: ethValue,
+              }),
+          ),
+          getArbitrumContracts(hre.network.name).inbox,
+          ethers.provider,
+          l2Provider,
+      );
+    });
